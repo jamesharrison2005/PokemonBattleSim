0540a2da2be417189416c932c3ff9e862c588503
James Harrison <jamesharrison2262@gmail.com>
2025-06-06 19:53:18 +0100

    Player 1 can now pick 2 pokemon and display stats

--- main.py
+++ main.py
@@ -14,9 +14,30 @@ def Get_Poke_Data(name):
         print(f"There was a problem retrieving data! Error Code: {response.status_code}")
     
     
+def MainMenu():
+    
+    p1 = []
 
-poke_Name = input("Please enter the name of a pokemon: ")
-poke_Data = Get_Poke_Data(poke_Name)
+    print("Welcome to the Pokemon Battle Sim!")
+    ctr = 1
+    for i in range(2):
+        print(f"Please enter the name of Pokemon {ctr} you want to use: ")
+        ctr += 1
+        name = input()
+        poke_Data = Get_Poke_Data(name)
+        if poke_Data:
+            pokemon = Pokemon(poke_Data)
+            p1.append(pokemon)  # Correct: Append to the p1 list        
+        
+    for pokemon in p1:
+        print("\n")
+        pokemon.Display_Stats()
+        
+    
+            
+    
+MainMenu()
 
-p1 = Pokemon(poke_Data)
-p1.Display_Stats()
+
+
+
